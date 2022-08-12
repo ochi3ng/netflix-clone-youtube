9532d8c171eae786ba768320cf0adbe5228d75bc
unknown <johnochi3ng@gmail.com>
2022-08-13 00:27:10 +0300

    changing on login page

--- src/Screens/SignupScreen.js
+++ src/Screens/SignupScreen.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { UseRef } from 'react'
+import { auth } from './firebase'
 import './SignupScreen.css'
 const signupScreen = () => {
+const emailRef=UseRef()
+const passwordRef=UseRef()
+
+const regester=(e)=>{
+  e.preventDefault()
+};
+const signIn=(e)=>{
+  e.preventDefault()
+  auth.creteUserWithEmailAndPassword(
+    emailRef.current.value,
+    passwordRef.current.value
+  ) .then((authUser)=>{
+    console.log(authUser)
+  })
+};
+
   return (
     <div className='SignupScreen'>
      <form>
       <h1>Sign in</h1>
       <input placeholder='Email' type="Email"/>
       <input placeholder='password' type="Password"/>
-      <button type='Submit'>Sign in</button>
+      <button type='Submit' onClick={signIn}>Sign in</button>
+
+
+
+
+      <h4>
+        <span className='SignupScreen_gray'>New to Netflix?</span>
+        <span className='SignupScreen_link'> Sign up now.</span>
+        </h4>
      </form>
     </div>
   )
--- src/Screens/firebase.js
+++ src/Screens/firebase.js
@@ -1,3 +1,4 @@
+import firebase from '../firebase'
 const firebaseConfig = {
     apiKey: "AIzaSyC1ZOBp-qCo0KhFnPd1L1t53GLX32McI4Q",
     authDomain: "netflix-clone-9f483.firebaseapp.com",
@@ -6,3 +7,9 @@ const firebaseConfig = {
     messagingSenderId: "420155961930",
     appId: "1:420155961930:web:c61bf714beeacfcab3ecb3"
   };
+  const firebaseApp=firebase.inializeApp(firebaseConfig);
+  const db=firebaseApp.fireStore();
+  const auth=firebase.auth();
+
+  export{auth}
+  export default db;
